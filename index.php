b7bfb9ecbb5f7fedfc245f607b53f14aab54e0f5
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2014-05-21 00:14:08 +0900

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP azlink
+Plugin URI: http://nafuel.net
+Description: AZlink-TinyWidget for Wordpress
+Author: Fumiya Nakamura
+Version: 0.1
+Author URI: http://nafuel.net
+*/
+?>
